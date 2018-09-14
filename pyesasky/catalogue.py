5ef6f67aa538aaaaf1e3c9d5da33198dd8b5136f
fgiordano <fgiordano@satl27.net4.lan>
2018-09-14 17:34:54 +0200

    Catalogue

--- pyesasky/catalogue.py
+++ pyesasky/catalogue.py
@@ -11,6 +11,7 @@ class Catalogue:
     _sources = []
     
     
+    
     def __init__(self, catalogueName, cooframe, color, lineWidth):
         _catalogueName = catalogueName
         _cooframe = cooframe
